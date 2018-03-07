2b096d56bfdcd56c8c4704d1ffb030abb521e42f
Harshit Raj <raj.0489@gmail.com>
2018-03-07 18:24:48 -0500

    implemented part and merge

--- ParallelSort/parallelSort/src/main/java/edu/info6205/parallelSort/ParSort.java
+++ ParallelSort/parallelSort/src/main/java/edu/info6205/parallelSort/ParSort.java
@@ -14,18 +14,18 @@ class ParSort {
     	if (size < cutoff) Arrays.sort(array, from, to);
         else {
             CompletableFuture<int[]> parsort1 = parsort(array, from, mid) ; // TODO implement me
-            CompletableFuture<int[]> parsort2 = parsort(array, mid+1,to); // TODO implement me
+            CompletableFuture<int[]> parsort2 = parsort(array, mid,to); // TODO implement me
             CompletableFuture<int[]> parsort = parsort1.
                     thenCombine(parsort2, (xs1, xs2) -> {
                         int[] result = new int[xs1.length + xs2.length];
                         System.out.println("In combine part");
                         System.out.println("Printing xs1");
                         for(int x : xs1) {
-                        	System.out.println(x);
+                        	//System.out.println(x);
                         }
                         System.out.println("Printing xs2---------------------------------------------");
                         for(int x : xs2) {
-                        	System.out.println(x);
+                        	//System.out.println(x);
                         }
                         int x1 = 0, x2 = 0, rs = 0;
                         int lxs1 = xs1.length, lxs2 = xs2.length, lrs = result.length;
@@ -33,12 +33,12 @@ class ParSort {
                         while(x1<lxs1 && x2< lxs2) {
                         	
                         	if(xs1[x1]<=xs2[x2]) {
-                        		System.out.println("Compared "+xs1[x1] + " , "+xs2[x2] );
+                        		//System.out.println("Compared "+xs1[x1] + " , "+xs2[x2] );
                         		result[rs] = xs1[x1];
                         		x1++;
                         	}
                         	else {
-                        		System.out.println("Compared "+xs1[x1] + " , "+xs2[x2] );
+                        		//System.out.println("Compared "+xs1[x1] + " , "+xs2[x2] );
                         		result[rs] = xs2[x2];
                         		x2++;
                         	}
@@ -76,13 +76,35 @@ class ParSort {
     private static CompletableFuture<int[]> parsort(int[] array, int from, int to) {
         return CompletableFuture.supplyAsync(
                 () -> {
-                    int[] result = new int[to  - from];
-                    for(int k = from; k<result.length; k++) {
-                    	result[k] =array[k];
-                    }
-                    //call sort again with the smaller array
-                    sort(result,0,result.length-1);
-                    return result;
+//                    for(int x: array) {
+//                    	System.out.println("psc "+x);
+//                    }
+//                	System.out.println("from : "+from+ "to : "+to+ "to - from : "+(to-from));
+//                	int[] result = new int[to  - from];
+//                	int k = from;
+//                	System.out.println("k starts at" + k);
+//                    for(; k<result.length; k++) {
+//                    	result[k] =array[k];
+//                    }
+//                    System.out.println("last k is : "+k);
+//                    //call sort again with the smaller array
+//                    
+//                    sort(result,0,result.length-1);
+//                    
+//                    System.out.println("-----------------------------------------------------");
+                	System.out.println("from : "+from+ "to : "+to+ "to - from : "+(to-from));
+                	int[] result = new int[to  - from];
+                	int i = 0;
+                	while(i < result.length) {
+                		result[i] = array[from+i];
+                		i++;
+                	}
+                
+                	//call sort again with the smaller array
+                  
+                    sort(result,0,result.length);
+                	
+                	return result;
                 }
         );
     }
